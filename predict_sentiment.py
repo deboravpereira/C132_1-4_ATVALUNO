f5ef41a26fa5d075a8ce9cb5af2cdad9219a65f7
deboravpereira <100171504+deboravpereira@users.noreply.github.com>
2023-08-27 22:20:30 -0300

    Add files via upload

---
+++ predict_sentiment.py
@@ -0,0 +1,35 @@
+import tensorflow.keras
+import numpy as np
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+sentence = ["estou me sentindo mal-humorado","eu não me senti humilhado"]
+
+# Tokenização
+tokenizer = Tokenizer(num_words=10000, oov_token='<OOV>')
+tokenizer.fit_on_texts(sentence)
+
+#Crie um dicionário chamado word_index
+word_index = tokenizer.word_index
+
+sequence = tokenizer.texts_to_sequences(sentence)
+print(sequence[0:2])
+
+# Preenchendo a sequência
+padded = pad_sequences(sequence, maxlen=100, padding='post', truncating='post')
+print(padded[0:2])
+
+# Defina o modelo usando um arquivo .h5
+model=tensorflow.keras.models.load_model('Analise_Ampla.h5')
+
+
+# Teste o modelo
+result=model.predict(padded)
+
+print(result)
+
+# Imprima o resultado
+predict_class = np.argmax(result, axis=1)
+print(predict_class)
+
+# {"alegria": 0, "medo": 1, "amor": 2, "tristeza": 3, "raiva": 4, "surpresa": 5}
